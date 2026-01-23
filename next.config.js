4daed8189d9e57c6b94fa71be8c655c295fbc5a4
Alan Yu <alan.yu2506@gmail.com>
2026-01-23 00:36:49 -0500

    Configure basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
 }
 
 module.exports = nextConfig
